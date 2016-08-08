cd6747dfa2563a315fff201884be8d851aed7890
Amio <amio.cn@gmail.com>
2016-08-08 16:15:10 +0800

    Add cli monitor.

---
+++ cli/help.js
@@ -0,0 +1,36 @@
+const path = require('path')
+
+const helpText = `
+  Usage
+
+    $ nace <command> <options>
+
+  Commands
+
+    start                 Start nace server.
+
+    Options
+
+    -s, --suffix <suffix> Save to a new file with <suffix>('submod' by default).
+    -f, --force           Save to original file.
+
+    Examples
+
+      # Decrease subtitle delay by 134.7 seconds:
+      $ submod sync *S5E01*.ass :-134.7
+
+      # Increase subtitle delay by 11.1 seconds:
+      $ submod sync *S5E01*.srt :+11.1
+`
+
+module.exports = {
+  printHelp: function () {
+    process.stdout.write(helpText)
+    return true
+  },
+  printVersion: function () {
+    const version = 'v' + require(path.join(__dirname, '../package.json')).version
+    process.stdout.write(version + '\n')
+    return true
+  },
+}
